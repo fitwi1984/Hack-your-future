2fc1ce796098a5bec8446862abc048114a16303b
fitwi1984 <64098023+fitwi1984@users.noreply.github.com>
2020-09-20 17:25:50 +0200

    Add files via upload

---
+++ customers.js
@@ -0,0 +1,48 @@
+function listCustomers(){
+let fname = document.getElementById('first').value
+let lname = document.getElementById('last').value
+let emaile = document.getElementById('email').value
+let password = document.getElementById('password').value
+
+let obj ={fname:fname, lname:lname, email:emaile, password:password}
+
+
+
+    let list = localStorage.getItem('list1')
+
+
+if(!list){
+    let arr =[]
+    arr.push(obj)
+    localStorage.setItem('list1', arr)
+}else{
+    let arr = localStorage.getItem('list1')
+
+    let emailExist = arr.find(element=> element.email===obj.email)
+
+        if(emailExist){
+      alert("Email already exist")
+      }else{
+     arr.push(obj)
+     localStorage.setItem('list1', arr)
+
+    alert("Successfully registered")
+
+        }
+
+console.log(arr)
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+}
